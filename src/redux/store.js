cef6f0fe54e9a205b9f283119d3c0e3f00869291
Zach Rustick <zachrustick@Zachs-MacBook-Pro.local>
2023-07-30 19:42:03 -0400

    removed redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
-import { composeWithDevTools } from 'redux-devtools-extension';
+//import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './rootReducer';
 
 import { legacy_createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-const enhancers = [applyMiddleware(thunk), composeWithDevTools()];
-//const enhancers = [applyMiddleware(thunk)];
+//const enhancers = [applyMiddleware(thunk), composeWithDevTools()];
+const enhancers = [applyMiddleware(thunk)];
 
 const store = legacy_createStore(
 
